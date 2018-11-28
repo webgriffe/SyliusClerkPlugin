428592057466d365d73c66605a883e9d7941fee0
Manuele Menozzi <manuele.menozzi@gmail.com>
2018-11-28 09:13:08 +0100

    Refactors products feed generation by extracting it in a service

--- src/Controller/FeedController.php
+++ src/Controller/FeedController.php
@@ -4,95 +4,35 @@ declare(strict_types=1);
 
 namespace Webgriffe\SyliusClerkPlugin\Controller;
 
-use Liip\ImagineBundle\Service\FilterService;
 use Sylius\Component\Core\Model\ChannelInterface;
-use Sylius\Component\Core\Model\ImageInterface;
-use Sylius\Component\Core\Model\ProductInterface;
-use Sylius\Component\Core\Model\ProductVariantInterface;
-use Sylius\Component\Core\Repository\ProductRepositoryInterface;
 use Sylius\Component\Locale\Model\LocaleInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
-use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Webgriffe\SyliusClerkPlugin\Service\ProductsFeedGenerator;
 use Webmozart\Assert\Assert;
 
 class FeedController extends Controller
 {
     /**
-     * @var FilterService
+     * @var ProductsFeedGenerator
      */
-    private $imagineFilterService;
+    private $productsFeedGenerator;
 
     /**
      * FeedController constructor.
      */
-    public function __construct(FilterService $imagineFilterService)
+    public function __construct(ProductsFeedGenerator $productsFeedGenerator)
     {
-        $this->imagineFilterService = $imagineFilterService;
+        $this->productsFeedGenerator = $productsFeedGenerator;
     }
 
     public function feedAction(int $channelId): Response
     {
         $channel = $this->getChannel($channelId);
         Assert::isInstanceOf($channel->getDefaultLocale(), LocaleInterface::class);
-        /** @var ProductRepositoryInterface $productRepository */
-        $productRepository = $this->get('sylius.repository.product');
-        /** @noinspection NullPointerExceptionInspection */
-        $localeCode = $channel->getDefaultLocale()->getCode();
-        $queryBuilder = $productRepository
-            ->createListQueryBuilder($localeCode)
-            ->andWhere(':channel MEMBER OF o.channels')
-            ->setParameter('channel', $channel)
-        ;
 
-        $defaultProductVariantResolver = $this->get('sylius.product_variant_resolver.default');
-
-        $productsData = [];
-        /** @var ProductInterface $product */
-        foreach ($queryBuilder->getQuery()->getResult() as $product) {
-            $productDefaultVariant = $defaultProductVariantResolver->getVariant($product);
-            if (!$productDefaultVariant) { // TODO test this
-                continue;
-            }
-            /** @var ProductVariantInterface $productDefaultVariant */
-            Assert::isInstanceOf($productDefaultVariant, ProductVariantInterface::class);
-            $channelPricing = $productDefaultVariant->getChannelPricingForChannel($channel);
-            if (!$channelPricing) { // TODO test this
-                continue;
-            }
-
-            $productData = [
-                'id' => $product->getId(),
-                'name' => $product->getName(),
-                'sku' => $product->getCode(),
-                'price' => $channelPricing->getPrice() / 100,
-                'url' => $this->generateUrl(
-                    'sylius_shop_product_show',
-                    ['slug' => $product->getSlug(), '_locale' => $product->getTranslation()->getLocale()],
-                    UrlGeneratorInterface::ABSOLUTE_URL
-                ),
-            ];
-            if ($product->getDescription()) { // todo test this
-                $productData['description'] = $product->getDescription();
-            }
-            $mainImage = $product->getImagesByType('main')->first();
-            if ($mainImage && $mainImage->getPath()) { // todo test this
-                /** @var ImageInterface $mainImage */
-                $productData['image'] = $this->imagineFilterService->getUrlOfFilteredImage(
-                    $mainImage->getPath(),
-                    'sylius_shop_product_thumbnail'
-                );
-            }
-            if ($channelPricing->getOriginalPrice()) { // todo test this
-                $productData['list_price'] = $channelPricing->getOriginalPrice() / 100;
-            }
-            $productData['categories'] = []; // todo implement/test categories
-
-            $productsData[] = $productData;
-        }
-
-        return new Response(json_encode(['products' => $productsData]));
+        return new Response(json_encode(['products' => $this->productsFeedGenerator->generate($channel)]));
     }
 
     /**
---
+++ src/Service/ProductsFeedGenerator.php
@@ -0,0 +1,107 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Webgriffe\SyliusClerkPlugin\Service;
+
+use Liip\ImagineBundle\Service\FilterService;
+use Sylius\Component\Core\Model\ChannelInterface;
+use Sylius\Component\Core\Model\ImageInterface;
+use Sylius\Component\Core\Model\ProductInterface;
+use Sylius\Component\Core\Model\ProductVariantInterface;
+use Sylius\Component\Core\Repository\ProductRepositoryInterface;
+use Sylius\Component\Locale\Model\LocaleInterface;
+use Sylius\Component\Product\Resolver\ProductVariantResolverInterface;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Component\Routing\RouterInterface;
+use Webmozart\Assert\Assert;
+
+class ProductsFeedGenerator
+{
+    /**
+     * @var ProductRepositoryInterface
+     */
+    private $productRepository;
+    /**
+     * @var ProductVariantResolverInterface
+     */
+    private $productVariantResolver;
+    /**
+     * @var FilterService
+     */
+    private $imagineFilterService;
+    /**
+     * @var RouterInterface
+     */
+    private $router;
+
+    public function __construct(
+        ProductRepositoryInterface $productRepository,
+        ProductVariantResolverInterface $productVariantResolver,
+        FilterService $imagineFilterService,
+        RouterInterface $router
+    ) {
+        $this->productRepository = $productRepository;
+        $this->productVariantResolver = $productVariantResolver;
+        $this->imagineFilterService = $imagineFilterService;
+        $this->router = $router;
+    }
+
+    public function generate(ChannelInterface $channel): array
+    {
+        Assert::isInstanceOf($channel->getDefaultLocale(), LocaleInterface::class);
+        /** @noinspection NullPointerExceptionInspection */
+        $localeCode = $channel->getDefaultLocale()->getCode();
+        $queryBuilder = $this->productRepository
+            ->createListQueryBuilder($localeCode)
+            ->andWhere(':channel MEMBER OF o.channels')
+            ->setParameter('channel', $channel)
+        ;
+
+        $productsData = [];
+        /** @var ProductInterface $product */
+        foreach ($queryBuilder->getQuery()->getResult() as $product) {
+            $productDefaultVariant = $this->productVariantResolver->getVariant($product);
+            if (!$productDefaultVariant) { // TODO test this
+                continue;
+            }
+            /** @var ProductVariantInterface $productDefaultVariant */
+            Assert::isInstanceOf($productDefaultVariant, ProductVariantInterface::class);
+            $channelPricing = $productDefaultVariant->getChannelPricingForChannel($channel);
+            if (!$channelPricing) { // TODO test this
+                continue;
+            }
+
+            $productData = [
+                'id' => $product->getId(),
+                'name' => $product->getName(),
+                'sku' => $product->getCode(),
+                'price' => $channelPricing->getPrice() / 100,
+                'url' => $this->router->generate(
+                    'sylius_shop_product_show',
+                    ['slug' => $product->getSlug(), '_locale' => $product->getTranslation()->getLocale()],
+                    UrlGeneratorInterface::ABSOLUTE_URL
+                ),
+            ];
+            if ($product->getDescription()) { // todo test this
+                $productData['description'] = $product->getDescription();
+            }
+            $mainImage = $product->getImagesByType('main')->first();
+            if ($mainImage && $mainImage->getPath()) { // todo test this
+                /** @var ImageInterface $mainImage */
+                $productData['image'] = $this->imagineFilterService->getUrlOfFilteredImage(
+                    $mainImage->getPath(),
+                    'sylius_shop_product_thumbnail'
+                );
+            }
+            if ($channelPricing->getOriginalPrice()) { // todo test this
+                $productData['list_price'] = $channelPricing->getOriginalPrice() / 100;
+            }
+            $productData['categories'] = []; // todo implement/test categories
+
+            $productsData[] = $productData;
+        }
+
+        return $productsData;
+    }
+}
