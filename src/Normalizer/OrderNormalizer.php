ea142b4c9716c95bdb98d21cdfe1a64c600ac210
Manuele Menozzi <manuele.menozzi@gmail.com>
2019-08-05 12:55:29 +0200

    Add OrderNormalizer for Clerk feed sales data

---
+++ src/Normalizer/OrderNormalizer.php
@@ -0,0 +1,61 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Webgriffe\SyliusClerkPlugin\Normalizer;
+
+use Sylius\Component\Core\Model\OrderInterface;
+use Sylius\Component\Core\Model\OrderItemInterface;
+use Symfony\Component\Serializer\Exception\InvalidArgumentException;
+use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
+
+final class OrderNormalizer implements NormalizerInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function normalize($object, $format = null, array $context = [])
+    {
+        if (!$object instanceof OrderInterface) {
+            throw new InvalidArgumentException('This normalizer supports only instances of ' . OrderInterface::class);
+        }
+        /** @var OrderInterface $order */
+        $order = $object;
+        $products = [];
+        /** @var OrderItemInterface $item */
+        foreach ($order->getItems() as $item) {
+            $productId = null;
+            $productVariant = $item->getVariant();
+            if ($productVariant) {
+                $product = $productVariant->getProduct();
+                if ($product) {
+                    $productId = $product->getId();
+                }
+            }
+            $products[] = [
+                'id' => $productId,
+                'quantity' => $item->getQuantity(),
+                'price' => $item->getUnitPrice() / 100,
+            ];
+        }
+
+        $checkoutCompletedAt = $order->getCheckoutCompletedAt();
+        $customer = $order->getCustomer();
+
+        return [
+            'id' => $order->getId(),
+            'products' => $products,
+            'time' => $checkoutCompletedAt ? $checkoutCompletedAt->getTimestamp() : null,
+            'email' => $customer ? $customer->getEmail() : null,
+            'customer' => $customer ? $customer->getId() : null,
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function supportsNormalization($data, $format = null)
+    {
+        return $data instanceof OrderInterface;
+    }
+}
--- src/Service/FeedGenerator.php
+++ src/Service/FeedGenerator.php
@@ -4,9 +4,12 @@ declare(strict_types=1);
 
 namespace Webgriffe\SyliusClerkPlugin\Service;
 
+use Doctrine\ORM\EntityRepository;
 use Sylius\Component\Core\Model\ChannelInterface;
+use Sylius\Component\Core\Model\OrderInterface;
 use Sylius\Component\Core\Model\ProductInterface;
 use Sylius\Component\Core\Model\TaxonInterface;
+use Sylius\Component\Core\Repository\OrderRepositoryInterface;
 use Sylius\Component\Core\Repository\ProductRepositoryInterface;
 use Sylius\Component\Locale\Model\LocaleInterface;
 use Sylius\Component\Taxonomy\Repository\TaxonRepositoryInterface;
@@ -24,6 +27,10 @@ class FeedGenerator
      */
     private $taxonRepository;
     /**
+     * @var OrderRepositoryInterface|EntityRepository
+     */
+    private $orderRepository;
+    /**
      * @var SerializerInterface
      */
     private $serializer;
@@ -31,10 +38,13 @@ class FeedGenerator
     public function __construct(
         ProductRepositoryInterface $productRepository,
         TaxonRepositoryInterface $taxonRepository,
+        OrderRepositoryInterface $orderRepository,
         SerializerInterface $serializer
     ) {
         $this->productRepository = $productRepository;
         $this->taxonRepository = $taxonRepository;
+        Assert::isInstanceOf($orderRepository, EntityRepository::class);
+        $this->orderRepository = $orderRepository;
         $this->serializer = $serializer;
     }
 
@@ -43,16 +53,23 @@ class FeedGenerator
         Assert::isInstanceOf($channel->getDefaultLocale(), LocaleInterface::class);
         /** @noinspection NullPointerExceptionInspection */
         $localeCode = $channel->getDefaultLocale()->getCode();
+        // TODO move query builders to standalone classes (which can be overridden in projects)
         $productsQueryBuilder = $this->productRepository
             ->createListQueryBuilder($localeCode)
             ->andWhere(':channel MEMBER OF o.channels')
             ->setParameter('channel', $channel)
         ;
         $taxonsQueryBuilder = $this->taxonRepository->createListQueryBuilder();
+        $ordersQueryBuilder = $this->orderRepository
+            ->createQueryBuilder('o')
+            ->where('o.channel', ':channel')
+            ->where('o.checkoutCompletedAt IS NOT NULL')
+        ;
 
         $feed = [
             'products' => [],
             'categories' => [],
+            'sales' => [],
             'created' => time(),
             'strict' => false,
         ];
@@ -63,6 +80,10 @@ class FeedGenerator
         /** @var TaxonInterface $taxon */
         foreach ($taxonsQueryBuilder->getQuery()->getResult() as $taxon) {
             $feed['categories'][] = $taxon;
+        }
+        /** @var OrderInterface $order */
+        foreach ($ordersQueryBuilder->getQuery()->getResult() as $order) {
+            $feed['sales'][] = $order;
         }
 
         return $this->serializer->serialize($feed, 'json', ['channel' => $channel]);
