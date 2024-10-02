e09295e43e8635048031e0c12781d6e64bd20e11
Lorenzo Ruozzi <lruozzi@webgriffe.com>
2024-10-02 14:19:11 +0200

    Change from xml to php tests service definition

---
+++ tests/Behat/Resources/services.php
@@ -0,0 +1,46 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Symfony\Component\DependencyInjection\Loader\Configurator;
+
+use Tests\Webgriffe\SyliusClerkPlugin\Behat\Context\Api\ClerkFeedContext;
+use Tests\Webgriffe\SyliusClerkPlugin\Behat\Context\Setup\ProductContext;
+use Tests\Webgriffe\SyliusClerkPlugin\Behat\Context\Shop\ClerkSalesTrackingContext;
+use Tests\Webgriffe\SyliusClerkPlugin\Behat\Context\Shop\ClerkTrackingCodeContext;
+use Tests\Webgriffe\SyliusClerkPlugin\Behat\Page\Shop\ThankYouPage;
+
+return static function (ContainerConfigurator $containerConfigurator) {
+    $services = $containerConfigurator->services();
+    $services->defaults()->public();
+
+    $services->set(ClerkFeedContext::class)
+        ->args([
+            service('test.client'),
+        ])
+    ;
+
+    $services->set(ProductContext::class)
+        ->args([
+            service('doctrine.orm.entity_manager'),
+            service('sylius.product_variant_resolver.default'),
+        ])
+    ;
+
+    $services->set(ClerkTrackingCodeContext::class)
+        ->args([
+            service('sylius.behat.page.shop.home'),
+        ])
+    ;
+
+    $services->set(ClerkSalesTrackingContext::class)
+        ->args([
+            service(ThankYouPage::class),
+        ])
+    ;
+
+    $services->set(ThankYouPage::class)
+        ->parent('sylius.behat.page.shop.order.thank_you')
+        ->private()
+    ;
+};
