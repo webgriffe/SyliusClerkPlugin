b68aaead88a3d1c034f7424630d73e08f7cbfcaf
Lorenzo Ruozzi <lruozzi@webgriffe.com>
2024-10-04 15:22:18 +0200

    Fix Sylius 1.12 compatibility

--- src/DataSyncInfrastructure/Controller/FeedController.php
+++ src/DataSyncInfrastructure/Controller/FeedController.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 namespace Webgriffe\SyliusClerkPlugin\DataSyncInfrastructure\Controller;
 
 if (!interface_exists(\Sylius\Resource\Doctrine\Persistence\RepositoryInterface::class)) {
-    class_alias(\Sylius\Resource\Doctrine\Persistence\RepositoryInterface::class, \Sylius\Component\Resource\Repository\RepositoryInterface::class);
+    class_alias(\Sylius\Component\Resource\Repository\RepositoryInterface::class, \Sylius\Resource\Doctrine\Persistence\RepositoryInterface::class);
 }
 use Sylius\Component\Channel\Repository\ChannelRepositoryInterface;
 use Sylius\Component\Core\Model\ChannelInterface;
