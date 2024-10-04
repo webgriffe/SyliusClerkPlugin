263108858d2c025ffc9e1c8270abfa315d293274
Lorenzo Ruozzi <lruozzi@webgriffe.com>
2024-10-04 14:30:34 +0200

    Add feed controller route

--- config/feed_routing.php
+++ config/feed_routing.php
@@ -3,10 +3,23 @@
 declare(strict_types=1);
 
 use Symfony\Component\Routing\Loader\Configurator\RoutingConfigurator;
+use Symfony\Component\Routing\Requirement\EnumRequirement;
 use Webgriffe\SyliusClerkPlugin\Controller\FeedController;
+use Webgriffe\SyliusClerkPlugin\DataSyncInfrastructure\Controller\FeedController as V2FeedController;
+use Webgriffe\SyliusClerkPlugin\DataSyncInfrastructure\Enum\Resource;
 
 return static function (RoutingConfigurator $routes): void {
     $routes->add('webgriffe_sylius_clerk_feed', '/clerk/feed/{channelId}')
         ->controller([FeedController::class, 'feedAction'])
+    ;
+
+    $routes->add('webgriffe_sylius_clerk_plugin_feed', '/clerk/feed/{channelCode}/{localeCode}/{resourceValue}')
+        ->controller(['webgriffe_sylius_clerk_plugin.controller.feed', '__invoke'])
+        ->methods(['GET'])
+        ->requirements([
+            'channelId' => '\d+',
+            'localeCode' => '^[A-Za-z]{2,4}(_([A-Za-z]{4}|[0-9]{3}))?(_([A-Za-z]{2}|[0-9]{3}))?$',
+            'resource' => new EnumRequirement(Resource::class),
+        ])
     ;
 };
---
+++ src/DataSyncInfrastructure/Controller/FeedController.php
@@ -0,0 +1,86 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Webgriffe\SyliusClerkPlugin\DataSyncInfrastructure\Controller;
+
+use Sylius\Component\Channel\Repository\ChannelRepositoryInterface;
+use Sylius\Component\Core\Model\ChannelInterface;
+use Sylius\Component\Locale\Model\LocaleInterface;
+use Sylius\Resource\Doctrine\Persistence\RepositoryInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Webgriffe\SyliusClerkPlugin\DataSyncInfrastructure\Enum\Resource;
+use Webgriffe\SyliusClerkPlugin\DataSyncInfrastructure\Generator\FeedGeneratorInterface;
+
+final class FeedController extends AbstractController implements FeedControllerInterface
+{
+    public function __construct(
+        private readonly ChannelRepositoryInterface $channelRepository,
+        private readonly RepositoryInterface $localeRepository,
+        private readonly FeedGeneratorInterface $productsFeedGenerator,
+        private readonly FeedGeneratorInterface $categoriesFeedGenerator,
+        private readonly FeedGeneratorInterface $ordersFeedGenerator,
+        private readonly FeedGeneratorInterface $customersFeedGenerator,
+        private readonly FeedGeneratorInterface $pagesFeedGenerator,
+    ) {
+    }
+
+    public function __invoke(
+        string $channelCode,
+        string $localeCode,
+        string $resourceValue,
+        Request $request,
+    ): Response {
+        $channel = $this->channelRepository->findOneByCode($channelCode);
+        if (!$channel instanceof ChannelInterface) {
+            throw $this->createNotFoundException();
+        }
+        $locale = $this->localeRepository->findOneBy(['code' => $localeCode]);
+        if (!$locale instanceof LocaleInterface) {
+            throw $this->createNotFoundException();
+        }
+        if (!$channel->hasLocale($locale)) {
+            throw $this->createNotFoundException();
+        }
+        $resource = Resource::tryFrom($resourceValue);
+        if ($resource === null) {
+            throw $this->createNotFoundException();
+        }
+
+        $feedGenerator = match ($resource) {
+            Resource::PRODUCTS => $this->productsFeedGenerator,
+            Resource::CATEGORIES => $this->categoriesFeedGenerator,
+            Resource::ORDERS => $this->ordersFeedGenerator,
+            Resource::CUSTOMERS => $this->customersFeedGenerator,
+            Resource::PAGES => $this->pagesFeedGenerator,
+        };
+        $modifiedAfter = null;
+        if ($request->query->has('modified_after')) {
+            $modifiedAfterDate = $request->query->getAlpha('modified_after');
+            $modifiedAfter = new \DateTimeImmutable($modifiedAfterDate);
+        }
+        $limit = null;
+        if ($request->query->has('limit')) {
+            $limit = $request->query->getInt('limit');
+        }
+        $offset = null;
+        if ($request->query->has('offset')) {
+            $offset = $request->query->getInt('offset');
+        }
+        $feed = $feedGenerator->generate(
+            $channel,
+            $localeCode,
+            $modifiedAfter,
+            $limit,
+            $offset,
+        );
+
+        return new Response(
+            $feed->getContent(),
+            Response::HTTP_OK,
+            ['Content-Type' => 'application/json'],
+        );
+    }
+}
